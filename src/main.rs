cfc8275bdb9e74794318ff849b56881505404aa3
George Slavin <george.r.slavin@gmail.com>
2017-05-02 16:08:55 -0400

    Replaced Hashset with a map of word counts
    Overhauled tests and methods to reflect this change.
    
    The original Hashset implementation didn't keep track of the frequency
    of the words used.

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use std::fs::File;
 use std::env;
-use std::cmp;
 
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::fmt;
+use std::cmp;
 
-// key words map to a word set
-type WordMap = HashMap<VecDeque<String>, HashSet<String>>;
+// key words map to a map of word counts
+type WordMap = HashMap<VecDeque<String>, HashMap<String, u64>>;
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct WordChain {
     key_length: usize,
@@ -19,8 +19,8 @@ struct WordChain {
 
 impl fmt::Display for WordChain {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for (key_words, word_set) in &self.word_map {
-            if let Err(x) = write!(f, "{:?} -> {:?})\n", key_words, word_set) {
+        for (key_words, word_counts) in &self.word_map {
+            if let Err(x) = write!(f, "{:?} -> {:?})\n", key_words, word_counts) {
                 return Err(x);
             }
         }
@@ -37,12 +37,8 @@ impl WordChain {
 
         let mut word_map: WordMap = HashMap::new();
         for word in text_iter {
-            if let None =  word_map.get_mut(&key_words) {
-                let word_set = HashSet::new();
-                word_map.insert(key_words.clone(), word_set);
-            }
-            let word_set = word_map.get_mut(&key_words).unwrap();
-            word_set.insert(word.to_string());
+            let mut word_count = word_map.entry(key_words.clone()).or_insert(HashMap::new());
+            (*(*word_count).entry(word.to_string()).or_insert(0)) += 1;
             key_words.pop_front();
             key_words.push_back(word.to_string());
         }
@@ -71,14 +67,24 @@ impl WordChain {
         }
 
         for key in &intersection {
-            let values: &HashSet<_> = self.word_map.get(&key).unwrap();
-            let values_other: &HashSet<_> = other.word_map.get(&key).unwrap();
-            let intersection_size =
-                (*values).intersection(values_other).count() as f64;
-            let max_size = cmp::max(values.len(), values_other.len());
-            result += intersection_size / (max_size as f64);
+            let word_counts: &HashMap<String, u64> = self.word_map.get(&key).unwrap();
+            let word_counts_other: &HashMap<String, u64> = other.word_map.get(&key).unwrap();
+            let words: HashSet<_> = word_counts.keys().collect();
+            let words_other: HashSet<_> = word_counts_other.keys().collect();
+            let mut intersection_size = 0;
+
+            for word in words.intersection(&words_other) {
+               intersection_size += *word_counts.get(word as &str).unwrap();
+               intersection_size += *word_counts_other.get(word as &str).unwrap();
+            }
+
+            let max_size: u64 = cmp::max(word_counts.values().sum(), word_counts_other.values().sum());
+
+            // Create a normailized sum so identical texts have a similarity of 1
+            result += (intersection_size as f64) / ((2 * max_size) as f64);
         }
 
+        // Create a normailized sum so identical texts have a similarity of 1
         result = result / (intersection.len() as f64);
 
         return Some(result);
@@ -90,10 +96,12 @@ impl WordChain {
         let word_maps = vec![&self.word_map, &other.word_map];
 
         for word_map in word_maps {
-            for (key, value_set) in word_map.clone() {
-                let mut entry = new_word_map.entry(key).or_insert(HashSet::new());
-                for v in value_set {
-                    entry.insert(v);
+            for (key, word_counts) in word_map.clone() {
+                let mut new_word_count =
+                    new_word_map.entry(key).or_insert(HashMap::new());
+                for word in word_counts.keys() {
+                   (*new_word_count.entry(word.clone()).or_insert(0)) +=
+                       *word_counts.get(word).unwrap();
                 }
             }
         }
@@ -142,7 +150,32 @@ fn word_chain_new() {
 }
 
 #[test]
-fn word_chain_compare() {
+fn word_chain_new_repeat_words() {
+    const KEY_LENGTH: usize = 2;
+    let text = String::from("This is a This is a");
+
+    let word_chain = WordChain::new(text, KEY_LENGTH);
+
+    let key_words: VecDeque<String> = vec![String::from("This"), String::from("is")].into_iter().collect();
+
+    let expected_word_counts: HashMap<String, u64> = vec![(String::from("a"), 2)].into_iter().collect();
+    let word_counts = word_chain.word_map.get(&key_words).unwrap();
+    assert_eq!(expected_word_counts, *word_counts);
+}
+
+#[test]
+fn word_chain_compare_eq() {
+    const KEY_LENGTH: usize = 2;
+    let text = String::from("This is a test");
+
+    let word_chain = WordChain::new(text, KEY_LENGTH);
+
+
+    assert_eq!(word_chain.compare(&word_chain.clone()).unwrap(), 1.0);
+}
+
+#[test]
+fn word_chain_compare_ne() {
     const KEY_LENGTH: usize = 2;
     let text = String::from("This is a test");
     let text_2 = String::from("No common words in text");
@@ -150,7 +183,7 @@ fn word_chain_compare() {
     let word_chain = WordChain::new(text, KEY_LENGTH);
     let word_chain_2 = WordChain::new(text_2, KEY_LENGTH);
 
-    assert_ne!(word_chain, word_chain_2);
+    assert_eq!(word_chain.compare(&word_chain_2).unwrap(), 0.0);
 }
 
 #[test]
@@ -178,9 +211,8 @@ fn word_chain_merge_common() {
 
     let key_words: VecDeque<String> = vec![String::from("This"), String::from("is")].into_iter().collect();
 
-    let mut expected_word_set = HashSet::new();
-    expected_word_set.insert(String::from("a"));
-    expected_word_set.insert(String::from("the"));
-    let word_set = new_word_chain.word_map.get(&key_words).unwrap();
-    assert_eq!(expected_word_set, *word_set);
+    let expected_word_counts: HashMap<String, u64> =
+        vec![(String::from("a"), 1), (String::from("the"), 1)].into_iter().collect();
+    let word_counts = new_word_chain.word_map.get(&key_words).unwrap();
+    assert_eq!(expected_word_counts, *word_counts);
 }
