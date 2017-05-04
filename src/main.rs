71a4412e5eeb440031a0b53ea986a8758e71c8d6
George Slavin <george.r.slavin@gmail.com>
2017-05-04 14:44:14 -0400

    Added title field to WordChain and cleaned up cmdline parsing

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,9 @@ use std::cmp;
 type WordMap = BTreeMap<VecDeque<String>, BTreeMap<String, u64>>;
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct WordChain {
-    key_length: usize,
-    word_map: WordMap
+    title: String,
+    word_map: WordMap,
+    key_length: usize
 }
 
 impl fmt::Display for WordChain {
@@ -30,7 +31,7 @@ impl fmt::Display for WordChain {
 
 impl WordChain {
     // Convert a block of text into a frequency histogram
-    fn new(text: String, key_length: usize) -> WordChain {
+    fn new(title: String, text: String, key_length: usize) -> WordChain {
         let mut text_iter = text.split_whitespace();
         let mut key_words: VecDeque<String> =
             text_iter.by_ref().take(key_length).map(|s| s.to_string()).collect();
@@ -43,7 +44,7 @@ impl WordChain {
             key_words.push_back(word.to_string());
         }
 
-        return WordChain{ key_length: key_length, word_map: word_map};
+        return WordChain{ title: title, word_map: word_map, key_length: key_length};
     }
 
     // Compare word chains
@@ -89,7 +90,10 @@ impl WordChain {
         return Some(result);
     }
 
-    fn merge(&self, other: &WordChain) -> WordChain {
+    fn merge(&self, other: &WordChain) -> Option<WordChain> {
+        if self.key_length != other.key_length {
+            return None;
+        }
         // Creates a merged map from the two given WordChain references
         let mut new_word_map = BTreeMap::new();
         let word_maps = vec![&self.word_map, &other.word_map];
@@ -104,8 +108,7 @@ impl WordChain {
                 }
             }
         }
-
-        return WordChain{ key_length: self.key_length, word_map: new_word_map};
+        Some(WordChain{ title: self.title.clone(), word_map: new_word_map, key_length: self.key_length})
     }
 
     fn len(&self) -> usize {
@@ -119,22 +122,27 @@ fn main() {
         println!("author_identifier [text] [comparison text]");
         return;
     }
-    const KEY_LENGTH: usize = 2;
+    const KEY_LENGTH: usize = 3;
 
     let mut word_chains: Vec<WordChain> = Vec::new();
 
     for path in env::args().skip(1) {
-        let f = File::open(path).unwrap();
+        let f = File::open(path.clone()).unwrap();
         let mut f = BufReader::new(f);
         let mut text = String::new();
         f.read_to_string(&mut text).expect("Error reading file");
-        let word_chain = WordChain::new(text, KEY_LENGTH);
+        let word_chain = WordChain::new(path, text, KEY_LENGTH);
+
         word_chains.push(word_chain);
     }
 
     //println!("{}", word_chains[0]);
-    let sim = word_chains[0].compare(&word_chains[1]).expect("Unable to compare word chains");
-    println!("{}", sim);
+    let test_word_chain =  &word_chains[0];
+    println!("Comparisons for {}", test_word_chain.title);
+    for word_chain in word_chains.iter().skip(1) {
+        let sim = test_word_chain.compare(word_chain).expect("Unable to compare word chains");
+        println!("{}: {}", word_chain.title, sim);
+    }
 }
 
 
@@ -143,7 +151,7 @@ fn word_chain_new() {
     const KEY_LENGTH: usize = 2;
     let text = String::from("This is a test");
 
-    let word_chain = WordChain::new(text, KEY_LENGTH);
+    let word_chain = WordChain::new(String::from("snippet"), text, KEY_LENGTH);
 
     let key_words: VecDeque<String> = vec![String::from("This"), String::from("is")].into_iter().collect();
 
@@ -157,7 +165,7 @@ fn word_chain_new_repeat_words() {
     const KEY_LENGTH: usize = 2;
     let text = String::from("This is a This is a");
 
-    let word_chain = WordChain::new(text, KEY_LENGTH);
+    let word_chain = WordChain::new(String::from("snippet"), text, KEY_LENGTH);
 
     let key_words: VecDeque<String> = vec![String::from("This"), String::from("is")].into_iter().collect();
 
@@ -171,7 +179,7 @@ fn word_chain_compare_eq() {
     const KEY_LENGTH: usize = 2;
     let text = String::from("This is a test");
 
-    let word_chain = WordChain::new(text, KEY_LENGTH);
+    let word_chain = WordChain::new(String::from("snippet"), text, KEY_LENGTH);
 
 
     assert_eq!(word_chain.compare(&word_chain.clone()).unwrap(), 1.0);
@@ -183,8 +191,8 @@ fn word_chain_compare_ne() {
     let text = String::from("This is a test");
     let text_2 = String::from("No common words in text");
 
-    let word_chain = WordChain::new(text, KEY_LENGTH);
-    let word_chain_2 = WordChain::new(text_2, KEY_LENGTH);
+    let word_chain = WordChain::new(String::from("snippet"), text, KEY_LENGTH);
+    let word_chain_2 = WordChain::new(String::from("snippet"), text_2, KEY_LENGTH);
 
     assert_eq!(word_chain.compare(&word_chain_2).unwrap(), 0.0);
 }
@@ -195,9 +203,9 @@ fn word_chain_merge() {
     let text = String::from("This is a test");
     let text_2 = String::from("No common words in text");
 
-    let word_chain = WordChain::new(text, KEY_LENGTH);
-    let word_chain_2 = WordChain::new(text_2, KEY_LENGTH);
-    let new_word_chain = word_chain.merge(&word_chain_2);
+    let word_chain = WordChain::new(String::from("snippet"), text, KEY_LENGTH);
+    let word_chain_2 = WordChain::new(String::from("snippet"), text_2, KEY_LENGTH);
+    let new_word_chain = word_chain.merge(&word_chain_2).unwrap();
 
     assert_eq!(new_word_chain.len(), word_chain.len() + word_chain_2.len());
 }
@@ -208,9 +216,9 @@ fn word_chain_merge_common() {
     let text = String::from("This is a test");
     let text_2 = String::from("This is the test");
 
-    let word_chain = WordChain::new(text, KEY_LENGTH);
-    let word_chain_2 = WordChain::new(text_2, KEY_LENGTH);
-    let new_word_chain = word_chain.merge(&word_chain_2);
+    let word_chain = WordChain::new(String::from("snippet"), text, KEY_LENGTH);
+    let word_chain_2 = WordChain::new(String::from("snippet"), text_2, KEY_LENGTH);
+    let new_word_chain = word_chain.merge(&word_chain_2).unwrap();
 
     let key_words: VecDeque<String> = vec![String::from("This"), String::from("is")].into_iter().collect();
 
